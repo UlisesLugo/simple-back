436c4a2a5ebc6e1e31bb50b0663781f8cdb0d740
Ulises Lugo <uliseslf99@hotmail.com>
2021-04-09 10:15:40 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get("/", async (req: Request, res: Response) => {
+  console.log(req.ip);
+  console.log(req.socket.remoteAddress);
+  // console.log(req.headers.host);
+  console.log(req.headers["x-forwarded-for"]);
+  res.send("Hello world!");
+});
+
+// start the Express server
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
+});
